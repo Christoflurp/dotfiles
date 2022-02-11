7d9f3acacdd4d05d70345c3fe7442000aa573b9e
Chris Sweeney <chris.sweeney@shopify.com>
2022-02-11 14:45:14 -0500

    setup tweaks

--- setup.sh
+++ setup.sh
@@ -8,8 +8,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 ## copy dootfills to root
 
-ln -fs "${clone_dir}/.zshrc" "${HOME}/.zshrc"
+cp "${clone_dir}/.zshrc" "${HOME}/.zshrc"
 # ln -fs "${clone_dir}/.gitconfig" "${HOME}/.gitconfig"
 cp "${clone_dir}/.vimrc" "${HOME}/.vimrc"
 
-zsh
